744efa3149e1b84f78aa0339e90500954e0e8479
chanceDira <chancedesire60@gmail.com>
2024-06-22 15:32:42 +0200

    Feat: added images

--- src/containers/Show.tsx
+++ src/containers/Show.tsx
@@ -9,20 +9,44 @@ const Show = () => {
       </div>
       
       <div className=' flex flex-row gap-8 h-70 w-4/5'>
-        <div className=' bg-white w-2/5 p-2 rounded-md'>
-          <img src="stage.png" alt="" className='h-full w-full rounded-md object-cover' />
+        <div className=' relative bg-white w-2/5 rounded-md'>
+          <img src="artist_1.png" alt="" className='h-full w-full rounded-md object-cover' />
+          <div className=' text-white text-md flex font-light justify-center items-center absolute bg-gradient-to-t from-[#02529B] to-[#40414500]  h-full w-full rounded-md top-0 '>
+           <div className=' absolute bottom-4 text-justify w-4/5'>
+
+           we have been committed to providing top-quality education in music, art, and the creative arts.
+           </div>
+          </div>
         </div>
-        <div className=' bg-white w-3/5  p-2 rounded-md  '>
-          <img src="stage.png" alt="" className=' h-full w-full rounded-md object-cover ' />
+        <div className=' relative bg-white w-3/5  rounded-md  '>
+          <img src="artist_5.png" alt="" className=' h-full w-full rounded-md object-cover ' />
+          <div className=' text-white text-md flex font-light justify-center items-center absolute bg-gradient-to-t from-[#02529B] to-[#40414500]  h-full w-full rounded-md top-0 '>
+           <div className=' absolute bottom-4 text-justify w-4/5'>
+
+           we have been committed to providing top-quality education in music, art, and the creative arts.
+           </div>
+          </div>
         </div>
       </div>
 
       <div className=' flex flex-row gap-8 h-70 w-4/5 mt-4'>
-        <div className=' bg-white w-3/5  p-2 rounded-md  '>
-          <img src="stage.png" alt="" className=' h-full w-full rounded-md object-cover ' />
+        <div className=' relative bg-white w-3/5  rounded-md  '>
+          <img src="artist_4.png" alt="" className=' h-full w-full rounded-md object-cover ' />
+          <div className=' text-white text-md flex font-light justify-center items-center absolute bg-gradient-to-t from-[#02529B] to-[#40414500]  h-full w-full rounded-md top-0 '>
+           <div className=' absolute bottom-4 text-justify w-4/5'>
+
+           we have been committed to providing top-quality education in music, art, and the creative arts.
+           </div>
+          </div>
         </div>
-        <div className=' bg-white w-2/5 p-2 rounded-md'>
-          <img src="stage.png" alt="" className='h-full w-full rounded-md object-cover' />
+        <div className=' relative bg-white w-2/5 rounded-md'>
+          <img src="artist_2.png" alt="" className='h-full w-full rounded-md object-cover' />
+          <div className=' text-white text-md flex font-light justify-center items-center absolute bg-gradient-to-t from-[#02529B] to-[#40414500]  h-full w-full rounded-md top-0 '>
+           <div className=' absolute bottom-4 text-justify w-4/5'>
+
+           we have been committed to providing top-quality education in music, art, and the creative arts.
+           </div>
+          </div>
         </div>
       </div>
       
